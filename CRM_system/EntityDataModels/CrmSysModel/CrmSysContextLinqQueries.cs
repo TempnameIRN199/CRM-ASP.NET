a696b2eac5d788299383d30c591e4ca8e8efd5be
TempnameIRN199 <vityayuzbekov602@gmail.com>
2024-05-28 17:46:33 +0300

    save

--- CRM_system/EntityDataModels/CrmSysModel/CrmSysContextLinqQueries.cs
+++ CRM_system/EntityDataModels/CrmSysModel/CrmSysContextLinqQueries.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace CRM_system.Models.EntityDataModels.CrmSysModel
--- CRM_system/Program.cs
+++ CRM_system/Program.cs
@@ -1,9 +1,16 @@
+using CRM_system.Models.EntityDataModels.CrmSysModel;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+string connection = builder.Configuration.GetConnectionString("master");
+
+builder.Services.AddDbContext<CrmSysContext>(options => options.UseSqlServer(connection));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

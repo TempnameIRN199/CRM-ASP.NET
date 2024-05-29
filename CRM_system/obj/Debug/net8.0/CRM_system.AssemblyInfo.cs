f3465f5bc46dba7062dccd16420ccff31255fca3
kuma <vityayuzbekov602@gmail.com>
2024-05-29 18:04:19 +0300

    update settings

--- CRM_system/obj/Debug/net8.0/CRM_system.AssemblyInfo.cs
+++ CRM_system/obj/Debug/net8.0/CRM_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f00a1614b96b845c5444516426ca2e6c1ee0768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a696b2eac5d788299383d30c591e4ca8e8efd5be")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

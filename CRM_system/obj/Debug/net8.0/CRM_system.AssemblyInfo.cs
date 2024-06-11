f7c19359dec1c9c50a8179422d757be5fc2e83d5
merphyth <senyachumak@gmail.com>
2024-06-11 23:09:05 +0200

    имя клиента в дитейлс пофиксил
    да это жоска

--- CRM_system/obj/Debug/net8.0/CRM_system.AssemblyInfo.cs
+++ CRM_system/obj/Debug/net8.0/CRM_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f6001f0bb4d90c990ddee945d8aacc8cb7dcbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0965fbb8cdceb9f4799e096092d19bd6cd1dd59")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
